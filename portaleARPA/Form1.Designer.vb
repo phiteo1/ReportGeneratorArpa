af450fddfa10d7a0f50a1cc76b222114b63a353e
phiteo1 <giuseppe.teodoro@argointech.com>
2024-08-21 15:44:20 +0200

    First Commit :)

---
+++ portaleARPA/Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(414, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(332, 45)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "GENERATORE REPORT"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.ComboBox1.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Camino 1", "Camino 2", "Camino 3", "Camino 4"})
+        Me.ComboBox1.Location = New System.Drawing.Point(90, 200)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(222, 21)
+        Me.ComboBox1.TabIndex = 1
+        Me.ComboBox1.Text = "Camino 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(84, 138)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 32)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Camino:"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Items.AddRange(New Object() {"Annuale", "Mensile"})
+        Me.ComboBox2.Location = New System.Drawing.Point(465, 200)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(223, 21)
+        Me.ComboBox2.TabIndex = 3
+        Me.ComboBox2.Text = "Annuale"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(459, 138)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(135, 32)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Granularità:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(84, 247)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 32)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Da:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(459, 247)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(34, 32)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "A:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(90, 478)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(302, 73)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Carica Dati"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(465, 478)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(265, 73)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Scarica report in Excel"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(829, 478)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(265, 73)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Scarica report in PDF"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(90, 618)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(200, 23)
+        Me.ProgressBar1.TabIndex = 12
+        Me.ProgressBar1.Visible = False
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.CustomFormat = "yyyy"
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(90, 300)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.ShowUpDown = True
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 13
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.CustomFormat = "yyyy"
+        Me.DateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker2.Location = New System.Drawing.Point(465, 300)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.ShowUpDown = True
+        Me.DateTimePicker2.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker2.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1188, 653)
+        Me.Controls.Add(Me.DateTimePicker2)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+End Class
---
+++ portaleARPA/Form1.vb
@@ -0,0 +1,156 @@
+﻿Imports System.Threading
+Imports System.Configuration
+Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim connectionString As String
+    Dim culture As System.Globalization.CultureInfo
+    Dim reportType As Int32
+    Dim section As Int32
+    Dim ret As Int32
+    Dim ret2 As Int32
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        connectionString = ConfigurationManager.ConnectionStrings("GLOBAL_CONN_STR").ConnectionString
+        DateTimePicker1.Value = Date.Now.AddYears(-1)
+        culture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US")
+        culture.NumberFormat.NumberGroupSeparator = ""
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        reportType = ComboBox2.SelectedIndex
+        section = 8
+        Dim startDate As Date = DateTimePicker1.Value
+        Dim endDate As Date = DateTimePicker2.Value
+
+        GetData(startDate, endDate, section, reportType)
+
+    End Sub
+
+    Private Async Sub GetData(startTime As Date, endTime As Date, section As Int32, type As Int32)
+
+        Dim command As System.Data.SqlClient.SqlCommand
+        Dim command2 As System.Data.SqlClient.SqlCommand
+        Dim reader As System.Data.SqlClient.SqlDataReader
+        Dim connection As New SqlConnection(connectionString)
+        Dim connection2 As New SqlConnection(connectionString)
+
+        Try
+            ' Tenta di aprire la connessione
+            connection.Open()
+            connection2.Open()
+            MessageBox.Show("Connessione al database riuscita!", "Successo", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Catch ex As Exception
+            ' Gestione degli errori
+            MessageBox.Show("Errore durante la connessione al database: " & ex.Message, "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End Try
+
+        Dim testCMD As Data.SqlClient.SqlCommand = New Data.SqlClient.SqlCommand("sp_AQMSNT_FILL_ARPA_MASSICI_CAMINI_NODELETE", connection)
+        testCMD.CommandTimeout = 18000
+        testCMD.CommandType = Data.CommandType.StoredProcedure
+        testCMD.Parameters.Add("@idsez", Data.SqlDbType.Int, 11)
+        testCMD.Parameters("@idsez").Direction = Data.ParameterDirection.Input
+        testCMD.Parameters("@idsez").Value = section
+        testCMD.Parameters.Add("@data", Data.SqlDbType.DateTime, 11)
+        testCMD.Parameters("@data").Direction = Data.ParameterDirection.Input
+        testCMD.Parameters("@data").Value = startTime
+        testCMD.Parameters.Add("@TIPO_ESTRAZIONE", Data.SqlDbType.Int, 11)
+        testCMD.Parameters("@TIPO_ESTRAZIONE").Direction = Data.ParameterDirection.Input
+        testCMD.Parameters("@TIPO_ESTRAZIONE").Value = reportType
+        testCMD.Parameters.Add("@retval", Data.SqlDbType.Int)
+        testCMD.Parameters("@retval").Direction = Data.ParameterDirection.Output
+
+        testCMD.ExecuteScalar()
+        ret = testCMD.Parameters("@retval").Value
+
+        testCMD.Parameters("@idsez").Value = 1
+        testCMD.ExecuteScalar()
+
+        ret2 = testCMD.Parameters("@retval").Value
+        Dim log_statement As String = "SELECT * FROM [ARPA_WEB_MASSICI_CAMINI] WHERE IDX_REPORT = " & ret.ToString() & " AND TIPO_DATO IS NULL ORDER BY INS_ORDER"
+        command = New System.Data.SqlClient.SqlCommand(log_statement, connection)
+        reader = command.ExecuteReader()
+        log_statement = "SELECT * FROM [ARPA_WEB_MASSICI_CAMINI] WHERE IDX_REPORT = " & ret2.ToString() & " AND TIPO_DATO IS NULL ORDER BY INS_ORDER"
+        command2 = New System.Data.SqlClient.SqlCommand(log_statement, connection2)
+        Dim reader2 As System.Data.SqlClient.SqlDataReader
+        reader2 = command2.ExecuteReader()
+
+        If (reader.HasRows) Then
+            While reader.Read()
+                reader2.Read()
+                Console.WriteLine(String.Format("Il valore NOX è: {0}", reader("E1Q_NOX")))
+                Console.WriteLine(String.Format("Il valore SO2 è: {0}", reader("E1Q_SO2")))
+                Console.WriteLine(String.Format("Il valore SO2 è: {0}", reader("E1Q_POLVERI")))
+            End While
+        End If
+
+
+        Button1.Enabled = False
+
+
+
+        'ProgressBar1.Location = New Point(465, 501)
+        'ProgressBar1.Visible = True
+
+
+        ' For i As Integer = 1 To 4
+        'Await Task.Delay(5000) ' Simula un lavoro lungo in modo asincrono
+        'ProgressBar1.Value += 25
+        'Next
+
+
+        'ProgressBar1.Visible = False
+
+        Button2.Visible = True
+        Button3.Visible = True
+
+    End Sub
+
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+
+        Dim startDate = DateTimePicker1.Value
+        Dim endDate = DateTimePicker2.Value
+
+        If endDate >= startDate Then
+            Button1.Enabled = True
+        Else
+            Button1.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub DateTimePicker2_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker2.ValueChanged
+
+        Dim startDate = DateTimePicker1.Value
+        Dim endDate = DateTimePicker2.Value
+
+        If endDate >= startDate Then
+            Button1.Enabled = True
+        Else
+            Button1.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+
+        Dim combobox As ComboBox = CType(sender, ComboBox)
+
+        If combobox.SelectedIndex = 1 Then
+            DateTimePicker1.CustomFormat = "MMMM yyyy"
+            DateTimePicker2.CustomFormat = "MMMM yyyy"
+
+        ElseIf combobox.SelectedIndex = 0 Then
+            DateTimePicker1.CustomFormat = "yyyy"
+            DateTimePicker2.CustomFormat = "yyyy"
+
+        End If
+
+    End Sub
+End Class
